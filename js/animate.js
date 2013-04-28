9168d79c5a947a997804eb42aee7188df4472089
Zach Schneider <me@zachschneider.com>
2013-04-28 15:56:24 -0500

    first init

---
+++ js/animate.js
@@ -0,0 +1,15 @@
+ // Animation Function
+function an_jumprope() {
+    var frameWidth = 510;
+    var frameHeight = 300;
+    var frames = 5;
+    var frame = 0;
+    var div = document.getElementById("an_jumprope");
+    setInterval(function () {
+        var frameOffset = (++frame % frames) * -frameWidth;
+        div.style.backgroundPosition = frameOffset + "px " + " 0px";
+    }, 140);
+}
+
+// firing off animation
+$("#an_jumprope").load(an_jumprope());
