78cb43264a0df4c99d45eec99c8474dc0328f31d
Zach Schneider <me@zachschneider.com>
2023-05-11 13:56:50 -0500

    Updated jQuery and refreshed js variables

--- js/animate.js
+++ js/animate.js
@@ -1,26 +1,28 @@
- // Animation Function
+// Animation Function
 function an_jumprope() {
-    var frameWidth = 510;
-    var frameHeight = 300;
-    var frames = 7;
-    var frame = 0;
-    var div = document.getElementById("an_jumprope");
-    setInterval(function () {
-        var frameOffset = (++frame % frames) * -frameWidth;
-        div.style.backgroundPosition = frameOffset + "px " + " 0px";
-    }, 140);
-}
-
-// firing off animation
-//$("#an_jumprope").load(an_jumprope());
-
-$(function(){
-  $("#an_jumprope").jBlitter({
-    'resource':'./img/an_jumprope.png',
-    'speed':100,
-    'frameWidth':510,
-    'frameHeight':300,
-    'reverse':false,
-    'loop':true,
+    const frameWidth = 510;
+    const frames = 7;
+    let frame = 0;
+    const div = document.getElementById("an_jumprope");
+    function animate() {
+      const frameOffset = (++frame % frames) * -frameWidth;
+      div.style.backgroundPosition = `${frameOffset}px 0px`;
+      requestAnimationFrame(animate);
+    }
+    animate();
+  }
+  
+  // firing off animation
+  an_jumprope();
+  
+  $(function () {
+    $("#an_jumprope").jBlitter({
+      resource: "./img/an_jumprope.png",
+      speed: 100,
+      frameWidth: 510,
+      frameHeight: 300,
+      reverse: false,
+      loop: true,
     });
-});
+  });
+  
